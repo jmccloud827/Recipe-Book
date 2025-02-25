956e5babbb1ee4c85849fc41d88b629118b1130a
Jacob McCloud <jmccloud@uwm.com>
2025-02-25 15:00:29 -0500

    Initial Commit

---
+++ Recipe Book/Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BookApp.swift
+//  Recipe Book
+//
+//  Created by Jacob McCloud on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
